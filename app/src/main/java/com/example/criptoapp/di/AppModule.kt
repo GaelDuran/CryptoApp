1026b7342a4e18ec03ef9d87c4b7513109f63503
GaelDuran <ldt76578@lasallebajio.edu.mx>
2025-05-30 17:25:48 -0600

    feat: Add Retrofit and AuthService DI
    This commit introduces dependency injection for Retrofit and an AuthService.
    
    - Added a provider for Retrofit instance, configured with a base URL and GsonConverterFactory.
    - Added a provider for AuthService, which uses the provided Retrofit instance.

--- app/src/main/java/com/example/criptoapp/di/AppModule.kt
+++ app/src/main/java/com/example/criptoapp/di/AppModule.kt
@@ -1,9 +1,12 @@
 package com.example.criptoapp.di
 
+import com.example.criptoapp.services.AuthServices
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -15,5 +18,20 @@ object AppModule {
     @Singleton
     fun providesATestingString(): String {
         return "Hola ando probando dagger hilt"
+    }
+
+    @Provides
+    @Singleton
+    fun providesRetrofitInstance() : Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesAuthService(retrofit: Retrofit): AuthServices {
+        return retrofit.create(AuthServices::class.java)
     }
 }
